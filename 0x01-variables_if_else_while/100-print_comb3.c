d905ae03c6d121a5b9a27205d45c379f8126be2d
Loïc Kami <lkami@bogital.com>
2023-07-15 01:58:02 +0000

    Added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints combinations of two digits with putchar
+ * Return: 0
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 0; i < 9; i++)
+{
+	for (j = i + 1; j < 10; j++)
+	{
+		putchar('0' + i);
+		putchar('0' + j);
+
+		if (i != 8 || j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
