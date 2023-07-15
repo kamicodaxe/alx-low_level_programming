f951c497f5b6159f54a42bd8badc50e17f3951d2
Loïc Kami <lkami@bogital.com>
2023-07-15 00:46:31 +0000

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers in base 10
+ * Return: 0
+ */
+int main(void)
+{
+int number;
+
+for (number = 0; number <= 9; number++)
+{
+	printf("%d", number);
+}
+printf("\n");
+return (0);
+}
