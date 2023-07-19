78714197b6dd02cb88ea7548919a5ee657c06a70
Loïc Kami <lkami@bogital.com>
2023-07-19 02:25:19 +0000

    Added _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an interger
+ * @n: int to compute absolute value
+ * Return: The absolute value of the function
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	return (-n);
+
+}
