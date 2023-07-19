018430f515045451fe96e39d4b329c02fd3fbe9d
Loïc Kami <lkami@bogital.com>
2023-07-19 01:37:27 +0000

    Added print_alphabet_x10 function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10x in terminal with  _putchar function
+ * Return: 0 (Always)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	while (i < 9)
+	{
+		print_alphabet();
+		i++
+	}
+
+}
