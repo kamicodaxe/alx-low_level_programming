ce10b11e764c6eb3ecfddeb906c23b69d953ff34
Loïc Kami <lkami@bogital.com>
2023-07-19 02:06:48 +0000

    Final fix 08 - Betty on print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet 10x in terminal with  _putchar function
+ * print_alphabet_x10 - Prints the alphabet 10x on terminal
  * Return: 0 (Always)
  */
 void print_alphabet_x10(void)
 {
 	int j = 0;
 	int i;
+
 	while (j < 10)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 		{
-		       	_putchar(i);
+			_putchar(i);
 		}
 		j++;
 		_putchar('\n');
