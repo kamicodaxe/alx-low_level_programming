1c630d43316330cd18da96fe8d68afa862edcfe9
Loïc Kami <lkami@bogital.com>
2023-07-20 03:00:08 +0000

    added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+
+/**
+ * main TODO
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+	int a;
+	int b = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			b = a + b;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
