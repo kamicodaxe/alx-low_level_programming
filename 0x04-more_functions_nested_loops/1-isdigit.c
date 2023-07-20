ac51573d1c17e59e574f399a278f5de12d58b495
Loïc Kami <lkami@bogital.com>
2023-07-20 21:21:30 +0000

    Added 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if c is a digit
+ *  @c: int to test
+ * Return: 1 - digit, 0 - is not a digit
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	return (0);
+}
