3635f4960a9833be16421098e539f6ff2428a893
Loïc Kami <lkami@bogital.com>
2023-07-20 22:45:59 +0000

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - returns the product of two intergers: a & b.
+ * @a: 1st interger
+ * @b: 2nd interger
+ * Return: 1 - digit, 0 - is not a digit
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
