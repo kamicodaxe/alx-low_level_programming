d6d0f18dd39a1dad68ca6a48388383ced39e54b4
Loïc Kami <lkami@bogital.com>
2023-07-21 01:21:42 +0000

    Added 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square.
+ * @size: int, size of sqaure
+ * Return: void
+ */
+void print_square(int size)
+{
+	const char NEW_LINE = '\n';
+	const char SQUARE_CHAR = '#';
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(SQUARE_CHAR);
+		}
+		_putchar(NEW_LINE);
+	}
+}
