5c9be912a21031661978497c515650b2035537d6
Loïc Kami <lkami@bogital.com>
2023-07-21 02:10:59 +0000

    Added 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor if 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	const long int NUMBER = 612852475143;
+	int i;
+
+	for (i = NUMBER / 2; i > 0; i--)
+	{
+		if (NUMBER % i == 0)
+		{
+			printf("%d", i);
+			break;
+		}
+	}
+	printf("\n");
+	return (0);
+}
