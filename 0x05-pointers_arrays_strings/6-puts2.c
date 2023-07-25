7a44f96f949a8276e9d6c181d7731de59a0c0376
Loïc Kami <lkami@bogital.com>
2023-07-25 05:10:26 +0000

    6-puts2.c upgrade

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,24 +10,11 @@
 void puts2(char *str)
 {
 	int len = 0;
-	int i;
-	char *start = str;
 
-	while (*str != '\0')
+	while (str[len] != '\0')
 	{
+		len % 2 == 0 ? _putchar(str[len]) : 0;
 		len++;
-		str++;
-	}
-
-	str = start;
-	for (i = 0; i < len; i++)
-	{
-		if (i % 2 != 0 || *str == '\0')
-			continue;
-		else
-		{
-			_putchar(str[i]);
-		}
 	}
 	_putchar('\n');
 }
