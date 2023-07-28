c42350666dbebdd6f1049b5c9471a5c25042fbb5
Loïc Kami <lkami@bogital.com>
2023-07-28 01:17:47 +0000

    Added 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+* @s1: char
+* @s2: char
+* Return: int
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
