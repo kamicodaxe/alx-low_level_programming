7df4d59be8142d3e8a278bd6e83a1975f5cb7a53
Loïc Kami <lkami@bogital.com>
2023-07-31 21:29:20 +0000

    Added 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * hasChar - checks if char is in string provided
+ * @s: string provided
+ * @c: char to test
+ * Return: 1 if c is in s, 0 otherwise
+ */
+int hasChar(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (1);
+		s++;
+	}
+	return (0);
+}
+
+
+/**
+ * _strspn - Searches a string for any of a set of bytes.
+ * @s: Pointer to the string to be searched
+ * @accept: Pointer to the string containing bytes to be matched
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		if (hasChar(accept, *s))
+			return (s);
+		s++;
+	}
+
+	return NULL;
+}
