4ee8f46e52f509db3bea78755a9bbf884bdad01c
Loïc Kami <lkami@bogital.com>
2023-08-01 02:53:18 +0000

    5-strstr.c fix 03

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,49 +1,36 @@
 #include "main.h"
 
 /**
- * isSubString - Checks if 'start' is actually the start of the matching
- * substring.
- * @start: Pointer to the potential start of the substring.
- * @needle: Pointer to the substring to be located.
- * Return: 1 if 'start' is the start of the matching substring, 0 otherwise.
- */
-int isSubString(char *start, char *needle)
-{
-	int i = 0;
-
-	for (i = 0; start[i] == needle[i]; i++)
-	{
-		if (needle[i] == '\0')
-			return (1);
-	}
-	return (0);
-}
-
-
-/**
  * _strstr - locates a substring in string.
  * @haystack: Pointer to the string to be searched
  * @needle: Pointer to the substring to be located.
  * Return: Pointer to the beginning of the located substring, or NULL if the
  * substring is not found.
  */
-
 char *_strstr(char *haystack, char *needle)
 {
+	int index;
 
-	if (*needle == '\0')
+	if (*needle == 0)
 		return (haystack);
 
-	while (*haystack != '\0')
+	while (*haystack)
 	{
-		if (*haystack == *needle)
+		index = 0;
+
+		if (haystack[index] == needle[index])
 		{
-			if (isSubString(haystack, needle) > 0)
-				return (haystack);
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
 		}
+
 		haystack++;
 	}
 
 	return ('\0');
 }
-
