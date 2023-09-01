04e4d627c85f9eca1681c784c8f62e3bd8700ef6
Loïc Kami wsl <lkami@bogital.com>
2023-09-01 12:41:47 +0200

    Added header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* HELPERS */
+int _putchar(char c);
+/* MAIN FUNCTIONS */
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+#endif /* MAIN_H */
