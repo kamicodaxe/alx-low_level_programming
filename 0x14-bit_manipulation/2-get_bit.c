6a5e1ea353c1a54db848e31627075d8037694667
Loïc Kami wsl <lkami@bogital.com>
2023-09-02 17:07:21 +0200

    fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,12 +8,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int mask;
+	unsigned long int mask;
 
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	mask = 1U << index;
+	mask = 1UL << index;
 
-	return (n & mask);
+	return ((n & mask) > 0);
 }
