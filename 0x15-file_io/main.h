ce59dbf60228a65b336029e90c61be63387a8d9a
Loïc Kami wsl <lkami@bogital.com>
2023-09-05 02:13:26 +0200

    Updated header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,4 +5,5 @@ int _putchar(char c);
 
 /* MAIN FUNCTIONS */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /* MAIN_H */
