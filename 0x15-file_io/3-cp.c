b0ed8f971f196ab9db3f4cc985f5c9abeea42f94
Loïc Kami wsl <lkami@bogital.com>
2023-09-05 04:16:58 +0200

    Fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,24 +18,23 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		puts("Usage: cp filename1 filename2\n");
-		printf("This program requires 2 arguments only\n");
-		return (-1);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (97);
 	}
 
 	src_fd = open(argv[1], O_RDONLY);
 	if (src_fd == -1)
 	{
-		printf("Could not read source file\n");
-		return (-1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
 	}
 
 	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (dest_fd == -1)
 	{
-		printf("Error reading dest file\n");
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		close(src_fd);
-		return (-1);
+		return (99);
 	}
 
 	result = copy_file(src_fd, dest_fd);
@@ -58,15 +57,22 @@ int copy_file(int source_fd, int dest_fd)
 {
 	char *buffer;
 	ssize_t bytes_read, bytes_written;
+	size_t buffer_size = 1024;
 
-	buffer = malloc(sizeof(char) * 1024);
-	while ((bytes_read = read(source_fd, buffer, 1024)) > 0)
+	buffer = malloc(sizeof(char) * buffer_size);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
+		return (-1);
+	}
+
+	while ((bytes_read = read(source_fd, buffer, buffer_size)) > 0)
 	{
 		bytes_written = write(dest_fd, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
 			free(buffer);
-			perror("Error: File I/O error\n");
+			dprintf(STDERR_FILENO, "Error: Can't write to file descriptor %d\n", dest_fd);
 			return (-1);
 		}
 	}
@@ -74,8 +80,8 @@ int copy_file(int source_fd, int dest_fd)
 	if (bytes_read == -1)
 	{
 		free(buffer);
-		perror("Error: File I/O error\n");
-		return (0);
+		dprintf(STDERR_FILENO, "Error: Can't read from file descriptor %d\n", source_fd);
+		return (100);
 	}
 
 	free(buffer);
