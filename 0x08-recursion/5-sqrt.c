7fae3787d455b316874d984f9ea45d4ff938db96
Loïc Kami <lkami@bogial.com>
2023-10-26 02:16:57 +0200

    Add _sqrt_recursion

---
+++ 0x08-recursion/5-sqrt.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * naturalSqrt - Computes the square root of a number using recursion
+ *
+ * This helper function calculates the square root of a given number 'num'
+ * recursively, starting from 'i' as the initial guess.
+ *
+ * @i: Current guess for square root
+ * @num: Number for which square root is to be calculated
+ *
+ * Return: Square root of 'num' if found, -1 otherwise.
+ */
+int naturalSqrt(int i, int num)
+{
+	if (i == 0)
+		return (-1);
+
+	if (i * i == num)
+		return (i);
+
+	return (naturalSqrt(i - 1, num));
+}
+
+/**
+ * _sqrt_recursion - Computes the square root of a non-negative integer
+ * using recursion
+ *
+ * This function calculates the square root of a non-negative integer
+ * 'n' recursively.
+ *
+ * @n: Non-negative integer for which square root is to be calculated
+ *
+ * Return: Square root of 'n', or -1 if 'n' is negative.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (naturalSqrt(n, n));
+}
