0ad0e4b255fd36794916589ed2bbe4412d071014
Loïc Kami <lkami@bogial.com>
2023-10-26 02:40:31 +0200

    Update wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,62 +1,23 @@
 #include "main.h"
 
-char *get_last_char_ptr(char *s1, char *s2)
-{
-	char *ptr = '\0';
-
-	while (*s1 != '\0')
-	{
-		if (*s1 == *s2)
-			ptr = s1;
-		s1++;
-	}
-	return (ptr);
-}
-
 /**
- * wildcmp - compares two strings.
- * @s1: string
- * @s2: string
+ * wildcmp - Compares two strings considering '*' as a wildcard
  *
- * Return: 1 if the strings can be considered identical, otherwise return 0.
+ * @s1: First string
+ * @s2: Second string with '*' as a wildcard character
+ *
+ * Return: 1 if strings can be considered identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0' && (*s2 == '\0' || (*s2 == '*' && *(s2 + 1) == '\0')))
 		return (1);
 
-	if (*s1 == '*')
-	{
-		while (*s1 == '*')
-		{
-			s1++;
-			if (*s1 == '\0')
-				return (1);
-		}
-
-		if (*s2 == '\0')
-			return (0);
-
-		return (wildcmp(get_last_char_ptr(s2, s1), s1));
-	}
-
 	if (*s2 == '*')
-	{
-		while (*s2 == '*')
-		{
-			s2++;
-			if (*s2 == '\0')
-				return (1);
-		}
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 
-		if (*s1 == '\0')
-			return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-		return (wildcmp(get_last_char_ptr(s1, s2), s2));
-	}
-
-	if (*s1 != *s2)
-		return (0);
-
-	return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
