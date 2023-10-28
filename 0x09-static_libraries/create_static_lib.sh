e3e890281189922e15970c67139f248a0d0532dd
Loïc Kami <lkami@bogial.com>
2023-10-28 03:53:39 +0200

    Update

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
-gcc -c *.c
-ar rcs liball.a *.o
+# Compile all .c files and create object files
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+
+# Create the static library from object files
+ar -rcs liball.a *.o
+
+# Index the static library
+ranlib liball.a
+
+# Clean up object files
+rm -f *.o
