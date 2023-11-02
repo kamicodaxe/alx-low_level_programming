30d770f8af3ff3d43f21029413991dce751340ef
Loïc Kami wsl <lkami@bogital.com>
2023-11-02 11:41:19 +0100

    Update on 100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -40,7 +40,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	newptr = malloc(new_size);
 	if (newptr == NULL)
-		return (NULL);
+	{
+		/* Free the original memory block */
+		free(ptr);
+		return NULL;
+	}
 
 	/* Copy old data to newptr */
 	for (i = 0; i < old_size; i++)
