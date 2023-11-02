bc0a75e1a6392a9f907e68d46ff35eaa1aaf578a
Loïc Kami wsl <lkami@bogital.com>
2023-11-02 11:48:17 +0100

    FIX 04

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	int *newptr;
 	size_t i;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
