8adbc8bfbb7ea386c65298beb4dfd4b6f6459c58
Loïc Kami wsl <lkami@bogital.com>
2023-11-07 02:46:32 +0100

    Add print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Prints information about a dog
+ * @d: Pointer to the struct dog to be printed
+ *
+ */
+void print_dog(struct dog *d)
+{
+	char *nil = "(nil)";
+
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n",
+		   d->name != NULL ? d->name : nil,
+		   d->age ? d->age : 0,
+		   d->owner != NULL ? d->owner : nil);
+}
