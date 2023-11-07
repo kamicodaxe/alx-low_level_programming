8816046940c70664261c978ddf2682340baabc93
Loïc Kami wsl <lkami@bogital.com>
2023-11-07 02:36:21 +0100

    Update dog.h, add betty description to struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Represents a dog with a name, age, and owner.
+ * @name: Pointer to the name of the dog.
+ * @age: Age of the dog.
+ * @owner: Pointer to the owner's name.
+ *
+ */
 struct dog
 {
 	char *name;
