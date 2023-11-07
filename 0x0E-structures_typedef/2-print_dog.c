9b918389a7d673bf0a19db0b4b5bcd140b57b4a9
Loïc Kami wsl <lkami@bogital.com>
2023-11-07 03:57:22 +0100

    FIX on 2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,9 @@ void print_dog(struct dog *d)
 {
 	char *nil = "(nil)";
 
+	if (d == NULL)
+		return;
+
 	printf("Name: %s\nAge: %0.6f\nOwner: %s\n",
 		   d->name != NULL ? d->name : nil,
 		   d->age >= 0 ? d->age : 0,
