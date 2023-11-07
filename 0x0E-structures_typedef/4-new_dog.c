bab3463162c9258e7928e06a46d926a18cb59788
Loïc Kami wsl <lkami@bogital.com>
2023-11-07 03:02:21 +0100

    Add 4-new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,62 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicates a string in memory.
+ * @str: String to duplicate.
+ * Return: Pointer to the duplicated string. NULL if str is NULL or if memory
+ * allocation fails.
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	size_t len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	/* Allocate memory for copy */
+	copy = malloc(len + 1);
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
+
+	copy[len + 1] = '\0';
+
+	return (copy);
+}
+
+/**
+ * new_dog - Creates a new dog structure.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ * Return: Pointer to the newly created dog_t structure.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new = malloc(sizeof(dog_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->name = _strdup(name);
+	if (new->name == NULL)
+		return (NULL);
+
+	new->owner = _strdup(owner);
+	if (new->owner == NULL)
+	{
+		free(new->name);
+		return (NULL);
+	}
+
+	new->age = age;
+
+	return (new);
+}
