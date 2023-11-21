5131e14b55c2a0ee5d81c20c00f89eaee9aab9e6
Loïc Kami wsl <lkami@bogital.com>
2023-11-21 23:53:06 +0100

    Fix double inclusion protection issue.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,13 +1,13 @@
-#include <stddef.h>
 #ifndef LIST_H
 #define LIST_H
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
