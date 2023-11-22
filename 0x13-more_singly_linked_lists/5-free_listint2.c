0957df4842451866033295c7de1e0e06b8eb6a81
Loïc Kami wsl <lkami@bogital.com>
2023-11-22 01:52:28 +0100

    free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Frees a linked list and sets head to NULL
+ * @head: Head of the list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+	head = NULL;
+}
