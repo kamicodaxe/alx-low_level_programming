8a09ab001421ac8d4aaf4efd6f0c42a1f9c87d2a
Loïc Kami wsl <lkami@bogital.com>
2023-11-22 22:48:31 +0100

    Update pop_listint for edge cases

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,9 +11,20 @@ int pop_listint(listint_t **head)
 	int n = 0;
 	listint_t *node_to_pop = *head;
 
-	if (!*head)
+	/* Empty list */
+	if (!head || !*head)
 		return (n);
 
+	/* one node only */
+	if (!node_to_pop->next)
+	{
+		n = node_to_pop->n;
+		free(node_to_pop);
+		head = NULL;
+		return (n);
+	}
+
+	n = node_to_pop->n;
 	*head = (*head)->next;
 
 	free(node_to_pop);
